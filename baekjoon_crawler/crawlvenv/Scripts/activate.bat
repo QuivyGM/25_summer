dec229411c7d12f6ed2702978c4a5621bce9f9bb
Quivy <minchan0902@gmail.com>
2025-08-16 11:04:02 +0900

    update gitignore

--- baekjoon_crawler/crawlvenv/Scripts/activate.bat
+++ baekjoon_crawler/crawlvenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\minch\programming\Personal\25_summer\baekjoon_crawler\crawlvenv
+set VIRTUAL_ENV=C:\Users\minch\Programming\25_summer\baekjoon_crawler\crawlvenv
 
 if not defined PROMPT set PROMPT=$P$G
 
